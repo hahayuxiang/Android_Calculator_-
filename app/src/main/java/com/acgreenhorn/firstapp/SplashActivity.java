4c4ccd38dc00f1aa9610d11eeb3501446881b40d
yuxiang <hahayuxiang@qq.com>
2022-10-12 20:19:32 +0800

    1012.01.03 ---添加了启动页面

---
+++ app/src/main/java/com/acgreenhorn/firstapp/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.acgreenhorn.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    // private final int SPLASH_DISPLAY_LENGHT = 2000; // 两秒后进入系统
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        //getSupportActionBar().hide();//隐藏标题栏
+        setContentView(R.layout.activity_splash);
+        Thread myThread=new Thread(){//创建子线程
+            @Override
+            public void run() {
+                try{
+                    sleep(1500);//使程序休眠五秒
+                    Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
+                    startActivity(it);
+                    finish();//关闭当前活动
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+
+    }
+}
